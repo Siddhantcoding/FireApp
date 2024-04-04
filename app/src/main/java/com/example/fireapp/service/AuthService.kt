35a09adb797eed5c6faab1cbd87c8e5c430f5f0b
siddhant <sidhantjha95@bbdu.ac.in>
2024-04-04 14:43:49 +0530

    Changes in fire app

--- app/src/main/java/com/example/fireapp/service/AuthService.kt
+++ app/src/main/java/com/example/fireapp/service/AuthService.kt
@@ -1,46 +1,44 @@
-package com.example.fireapp.service
+package com.digi.fireapp.service
 
+import android.util.Log
 import com.digi.fireapp.ui.screens.login.LoginState
-import com.example.fireapp.ui.screens.Register.RegisterState
+import com.digi.fireapp.ui.screens.register.RegisterState
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.auth.userProfileChangeRequest
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 
 class AuthService(
-   private val auth:FirebaseAuth= Firebase.auth,
+    private val auth: FirebaseAuth = Firebase.auth,
 ) {
-    fun login(state: MutableStateFlow<LoginState>){
-        auth.signInWithEmailAndPassword(state.email,state.password).addOnFailureListener{
-            state.error = it.message ?: "An error Occurred"
-            state.isLoading =false
-        } .addOnSuccessListener {
-            state.isLoading = false
-            state.error = ""
-            state.isLoginSuccess = true
-        }
-    }
-    fun register(state:RegisterState) {
-        auth.createUserWithEmailAndPassword(state.email, state.password)
+    fun login(state: MutableStateFlow<LoginState>) {
+        Log.d("AuthService", "login: ${state.value.email} ${state.value.password}")
+        auth.signInWithEmailAndPassword(state.value.email, state.value.password)
             .addOnFailureListener {
-                state.error = it.message ?: "An error Occurred"
-                state.isLoading = false
-            }.addOnSuccessListener {
-                state.isLoading = false
-                state.error = ""
-                state.isLoginSuccess = true
-                val profileUpdates = userProfileChangeRequest {
-                    displayName = state.username
+                state.update { state ->
+                    state.copy(error = it.message ?: "An error occurred", isLoading = false)
                 }
+            }.addOnSuccessListener {
+                state.update { state ->
+                    state.copy(isLoginSuccess = true, isLoading = false, error = "")
+                }
+            }
+    }
+
+    fun register(state: MutableStateFlow<RegisterState>) {
+        auth.createUserWithEmailAndPassword(state.value.email, state.value.password)
+            .addOnFailureListener {
+                state.update { state ->
+                    state.copy(error = it.message ?: "An error occurred", isLoading = false)
+                }
+            }.addOnSuccessListener {
+                state.update { state ->
+                    state.copy(isRegisterSuccess = true, isLoading = false, error = "")
+                }
+                val profileUpdates = userProfileChangeRequest { displayName = state.value.username }
                 it.user?.updateProfile(profileUpdates)
             }
     }
-    fun isUserLoggedIn():Boolean {
-        return auth.currentUser != null
-    }}
-
-
-
-
-
+}
